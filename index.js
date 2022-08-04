396fefd3c26858181130a16ca50d6a96fcb77a86
HungryJustice <54478428+HungryJustice@users.noreply.github.com>
2022-08-04 23:40:48 +0200

    commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const prefix = "!";
 //rajouter !clear @ -> clear tous les messages de @
 //@ = <@XXXXXXXXXXXXXXXXXX>
 client.once("ready", () => {
-    client.user.setPresence({ activities: [{ name: `de la haine.`, type: 1 }], status: 'dnd' })
+    client.user.setPresence({ activities: [{ name: `de la haine.`, type: ActivityType.Streaming }], status: 'dnd' })
     console.log(`Bot en ligne.`)
     const Startembed = new Discord.EmbedBuilder()
         .setColor("#0099ff")
