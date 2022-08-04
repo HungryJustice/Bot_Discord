ba0825d8d8efc131752029928e390c04b27893fb
HungryJustice <54478428+HungryJustice@users.noreply.github.com>
2022-08-04 22:46:56 +0200

    commit

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ client.on("messageCreate", message => {
                     let number = parseInt(args[1]);
                     if (args[1].startsWith("<@")) {
                         var trash = 0
-                        const messages = message.channel.messages.fetch() //.split("")
+                        const messages = message.channel.messages.fetch().catch(() => undefined); //.split("")
                         console.log(messages)
                         console.log(typeof messages)
                         messages.forEach(element => {
