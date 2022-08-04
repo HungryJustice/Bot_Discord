881b9846efe4fc1f1767a12298b55bbed97f4aa3
HungryJustice <54478428+HungryJustice@users.noreply.github.com>
2022-08-04 23:50:39 +0200

    commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
-const ActivityType = require('discord.js');
-const GatewayIntentBits = require('discord.js')
+const { ActivityType, GatewayIntentBits } = require('discord.js');
 const { joinVoiceChannel, AudioPlayerStatus, createAudioPlayer, createAudioResource, StreamType } = require('@discordjs/voice');
 const client = new Discord.Client({ intents: [Discord.IntentsBitField.Flags.Guilds, Discord.IntentsBitField.Flags.MessageContent, Discord.IntentsBitField.Flags.GuildMessages, Discord.IntentsBitField.Flags.GuildVoiceStates, Discord.IntentsBitField.Flags.GuildPresences] });
 client.login(process.env.TOKEN);
