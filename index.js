ff5e96164081ed730fb25e751f73a98dcb30738e
HungryJustice <54478428+HungryJustice@users.noreply.github.com>
2022-08-06 05:23:38 +0200

    commit

--- index.js
+++ index.js
@@ -67,7 +67,30 @@ client.on("messageCreate", message => {
                                 nargs.push(arg.slice(2, -1))
                             }
                         }
+                        //
+                        //
+                        const channel = client.channels.cache.get("<my-channel-id>");
+                        let messages = [];
+                        // Create message pointer
+                        let message = await channel.messages
+                            .fetch({ limit: 1 })
+                            .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
 
+                        while (message) {
+                            await channel.messages
+                                .fetch({ limit: 100, before: message.id })
+                                .then(messagePage => {
+                                    messagePage.forEach(msg => messages.push(msg));
+
+                                    // Update our message pointer to be last message in page of messages
+                                    message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
+                                })
+                        }
+                        console.log(messages); // Print all messages
+                        console.log(typeof messages); // Print all messages
+                        return;
+                        //
+                        //
                         var supprimés = 0
                         var a_supprimer = new Array()
                         for (const person of nargs) {
