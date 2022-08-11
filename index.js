90e75482225321a197d509421cd6c52fda169cff
HungryJustice <54478428+HungryJustice@users.noreply.github.com>
2022-08-11 11:56:52 +0200

    commit

--- index.js
+++ index.js
@@ -42,10 +42,25 @@ client.once("ready", () => {
 //     newmessage.reply("Vu !\n>>||" + oldmessage.content + "||")
 // })
 
-client.on('messageReactionAdd', (reaction, user) => {
-    user.send("test")
-    console.log(reaction)
-})
+client.on('messageReactionAdd', async(reaction, user) => {
+    // When a reaction is received, check if the structure is partial
+    if (reaction.partial) {
+        // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.error('Something went wrong when fetching the message:', error);
+            // Return as `reaction.message.author` may be undefined/null
+            return;
+        }
+    }
+
+    // Now the message has been cached and is fully available
+    console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+    // The reaction is now also fully available and the properties will be reflected accurately:
+    console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+});
+
 client.on("messageCreate", message => {
     if (message.content.startsWith(prefix)) {
         var index = 0;
