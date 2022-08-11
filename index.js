7a80573da7194b440eedc48ac9e3949b97ab4ddf
HungryJustice <54478428+HungryJustice@users.noreply.github.com>
2022-08-11 14:47:05 +0200

    commit

--- index.js
+++ index.js
@@ -110,7 +110,6 @@ client.on("messageCreate", message => {
                             m.react('1007234604480069662');
                             m.react('1007238080153006110');
                             const filter = (reaction, user) => {
-                                return (1 == 1)
                                 return (reaction.emoji.id === '1007234604480069662' || reaction.emoji.id === '1007238080153006110') && user.id === message.author.id;
                             };
                             m.awaitReactions({ filter, max: 3, time: 4000, errors: ['time'] })
