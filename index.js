73760be81e72592c317c06eed85ad817cda83c68
HungryJustice <54478428+HungryJustice@users.noreply.github.com>
2022-08-13 15:31:14 +0200

    commit

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const client = new Discord.Client({ intents: [Discord.IntentsBitField.Flags.Guil
 //essayer de remplacer le await reaction par le client.on('messageReactionAdd')
 
 const token = process.env.TOKEN
-console.log(client.emojis.cache.find(emoji => 1 == 1))
-const coche = client.emojis.cache.find(emoji => emoji.name === ":coche:");
+const coche = client.guild.emojis.cache.find(emoji => emoji.name === ":coche:");
 const croix = client.emojis.cache.find(emoji => emoji.name === ":coche:");
 var appName = 'botdiscordlouismazin';
 var tok = "27fcf1f1-b3d9-471a-8d5e-1d02b1014885"
