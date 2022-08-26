4d093e605d38447334d49d7d3aa32a3d0fb301d3
HungryJustice <54478428+HungryJustice@users.noreply.github.com>
2022-08-26 15:36:19 +0200

    commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var bloquer = false;
 client.login(token)
 
 client.once("ready", () => {
-    console.log(client.guilds.first().presences.get(client.user.id))
+    console.log(client)
     client.user.setPresence({ activities: [{ name: `de la haine.`, type: ActivityType.Streaming, url: "https://youtube.com/watch?v=dQw4w9WgXcQ" }], status: 'dnd' })
     console.log(`Bot en ligne.`)
 })
